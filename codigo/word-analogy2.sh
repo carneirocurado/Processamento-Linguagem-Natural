c61d7b9bd68915a5665001e2f485a3e50c1c7c7f
Vitor Curado <vitor.curado@gmail.com>
2018-11-05 10:37:35 -0200

    envio parcial

--- codigo/word-analogy2.sh
+++ codigo/word-analogy2.sh
@@ -9,10 +9,10 @@ timestamp(){
 
 printf "\n%s: Iniciando Execucao..." "$(timestamp)"
 
-QTDE_FILES=$(ls -1 ./corpus | wc -l)
+QTDE_FILES=$(ls -1 ./corpus2 | wc -l)
 qtde_processada=0
 
-for arquivo in ./corpus/*; do
+for arquivo in ./corpus2/*; do
 	qtde_processada=$(($qtde_processada+1))
 	printf "\n------------------------------\n%s: Corpus de Entrada: %s - %d de %d" "$(timestamp)" "$(basename $arquivo)" "$qtde_processada" "$QTDE_FILES"
 
@@ -31,23 +31,23 @@ for arquivo in ./corpus/*; do
 		then
 			printf "\nWARNING: Quantidade encontrada de palavras diferente da esperada! Pulando..."
 		else
-			for ((i=1; i<=50; i++)); do
-				if [ $i -gt 2 ]
-				then
-					i=$(($i+4))
-					printf "\n\n%s: Termino da Execucao\n" "$(timestamp)"
-					exit 0
-				fi
+			for ((i=2; i<=20; i=$(($i+2)))); do
+#				if [ $i -gt 20 ]
+#				then
+#					i=$(($i+4))
+#					printf "\n\n%s: Termino da Execucao\n" "$(timestamp)"
+#					exit 0
+#				fi
 				ARQ_WORDSPACE=./wordspaces/$(basename $arquivo)_cbow_context-$i.bin
 
 				printf "\n------------------------------\n%s: Iniciando Construcao do Wordspace: %s\n" "$(timestamp)" "$ARQ_WORDSPACE"
-				./word2vec/word2vec -train $arquivo -output $ARQ_WORDSPACE -cbow 1 -size 200 -window $i -negative 25 -threads 20 -binary 1 -iter 15
+				time ./word2vec/word2vec -train $arquivo -output $ARQ_WORDSPACE -cbow 1 -size 200 -window $i -negative 25 -threads 20 -binary 1 -iter 15
 				printf "\n\n%s: Iniciando Analise: %s\n" "$(timestamp)" "$ARQ_WORDSPACE"
 				./word-analogy2 $ARQ_WORDSPACE $ARQ_SAIDA < $ARQ_IN
 
 				ARQ_WORDSPACE=./wordspaces/$(basename $arquivo)_skip-gram_context-$i.bin
 				printf "\n------------------------------\n%s: Iniciando Construcao do Wordspace: %s\n" "$(timestamp)" "$ARQ_WORDSPACE"
-				./word2vec/word2vec -train $arquivo -output $ARQ_WORDSPACE -cbow 0 -size 200 -window $i -negative 25 -threads 20 -binary 1 -iter 15
+				time ./word2vec/word2vec -train $arquivo -output $ARQ_WORDSPACE -cbow 0 -size 200 -window $i -negative 25 -threads 20 -binary 1 -iter 15
 				printf "\n\n%s: Iniciando Analise: %s\n" "$(timestamp)" "$ARQ_WORDSPACE"
 				./word-analogy2 $ARQ_WORDSPACE $ARQ_SAIDA < $ARQ_IN
 			done
